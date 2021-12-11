76d097260392a0a347c2ad7a07c3b0c27c94bc8f
Paferreira982 <paferreira982@hotmail.com>
2021-12-11 20:27:58 -0300

    Correcoes no codigo

--- local_lib/commons.py
+++ local_lib/commons.py
@@ -26,24 +26,24 @@ async def printHelp(message):
     \t$price [token_name] -> Retorna o valor do token em BRL e Dolar em tempo real.\n
     \t$convert [quantity] [token_name] -> Converte um valor em token em BRL e Dolar.\n
     \t$tokens -> Imprime a lista de tokens cadastrados.
-    """
+    ```"""
     await printMsg(string, message)
 
 async def printPrice(command, message):
     token = getTokenInfo(command[1])
     usd = currency.getTokenQuote(token)
-    string = "``` Valor atual do {}\nUSD ->  $ {:.2f}\nBRL -> R$ {:.2f}```".format(token['symbol'], usd, currency.usdToBrl(usd))
+    string = "```Valor atual do {}\nUSD ->  $ {:.2f}\nBRL -> R$ {:.2f}```".format(token['symbol'], usd, currency.usdToBrl(usd))
     await printMsg(string, message)
 
 async def printConvert(command, message):
-    token = getTokenInfo(command[2].str.lower())
+    token = getTokenInfo(command[2].lower())
     usd = currency.getTokenQuote(token)
-    string = "``` {} {}'s\nUSD ->  $ {:.2f}\nBRL -> R$ {:.2f}```".format(command[1], token['symbol'], usd*float(command[1]), currency.usdToBrl(usd)*float(command[1]))
+    string = "```{} {}'s\nUSD ->  $ {:.2f}\nBRL -> R$ {:.2f}```".format(command[1], token['symbol'], usd*float(command[1]), currency.usdToBrl(usd)*float(command[1]))
     await printMsg(string, message)
 
 async def printTokens(message):
-    string = "``` Tokens cadastrados: "
+    string = "```Tokens cadastrados: "
     for tokenName in getTokenInfo("all"):
-        string += tokenName.str.lower() + ", "
+        string += tokenName.lower() + ", "
     string += "```"
     await printMsg(string, message)
