1c46d96f5bc171454445a434c93dbf6d999e45e3
Paferreira982 <paferreira982@hotmail.com>
2021-12-13 20:41:49 -0300

    Modificacoes no timer

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
   print("Logado como {0.user}".format(client))
-  timer1 = Timer(interval=1200, first_immediately=True, timer_name="Timer 1", client=client, callback=commons.statusInterval)
+  Timer(interval=1200, first_immediately=True, timer_name="Timer 1", client=client, callback=commons.statusInterval)
   
 #Função responsável por capturar a input do usuário e identificar qual comando foi enviado.
 @client.event
@@ -46,8 +46,8 @@ async def on_message(message):
     if msg.startswith("$tokens"):
       await commons.printTokens(message)
 
-loop = asyncio.get_event_loop()
-loop.run_forever()
-
 keep_alive()
 client.run(os.environ['token'])
+
+loop = asyncio.get_event_loop()
+loop.run_forever()
