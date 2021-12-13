50cb2a039f358612990b2f3ed74a47008be94a3c
Paferreira982 <paferreira982@hotmail.com>
2021-12-13 20:26:52 -0300

    Insercao da classe timer

---
+++ local_lib/timer.py
@@ -0,0 +1,26 @@
+import asyncio
+
+class Timer:
+    def __init__(self, interval, first_immediately, timer_name, context, callback):
+        self._interval = interval
+        self._first_immediately = first_immediately
+        self._name = timer_name
+        self._context = context
+        self._callback = callback
+        self._is_first_call = True
+        self._ok = True
+        self._task = asyncio.ensure_future(self._job())
+
+    async def _job(self):
+        try:
+            while self._ok:
+                if not self._is_first_call or not self._first_immediately:
+                    await asyncio.sleep(self._interval)
+                await self._callback(self._name, self._context, self)
+                self._is_first_call = False
+        except Exception as ex:
+            print(ex)
+
+    def cancel(self):
+        self._ok = False
+        self._task.cancel()
--- main.py
+++ main.py
@@ -2,22 +2,23 @@
 #Script principal: Gerencia os comandos proveniente do usuário.
 import sys
 import os
-from threading import Timer
+import asyncio
 
 #Insere a pasta "local_lib" no path do sistema, para tornar os scripts presentes visíveis.
 sys.path.insert(1, './local_lib')
 
 import discord
 import commons
+from timer import Timer
 from keep_alive import keep_alive
 
 client = discord.Client()
+timer1 = Timer(interval=1200, first_immediately=True, client=client, callback=commons.statusInterval)
 
 #Função responsável por informar se o login no BOT foi bem sucedido.
 @client.event
 async def on_ready():
   print("Logado como {0.user}".format(client))
-  Timer(1200, await commons.statusInterval(client)).start()
   
 #Função responsável por capturar a input do usuário e identificar qual comando foi enviado.
 @client.event
@@ -44,6 +45,9 @@ async def on_message(message):
 
     if msg.startswith("$tokens"):
       await commons.printTokens(message)
-  
+
+loop = asyncio.get_event_loop()
+loop.run_forever()
+
 keep_alive()
 client.run(os.environ['token'])
