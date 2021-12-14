674ec1476b2deee91a575e0688a1a7a9b8d007bc
Paferreira982 <paferreira982@hotmail.com>
2021-12-13 21:30:58 -0300

    Finalizacao do Timer

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ client = discord.Client()
 @client.event
 async def on_ready():
   print("Logado como {0.user}".format(client))
-  Timer(interval=1200, first_immediately=True, timer_name="Timer 1", cliente=client, callback=commons.statusInterval)
   
 #Função responsável por capturar a input do usuário e identificar qual comando foi enviado.
 @client.event
@@ -49,5 +48,6 @@ async def on_message(message):
 keep_alive()
 client.run(os.environ['token'])
 
+Timer(interval=1200, first_immediately=True, timer_name="Timer 1", cliente=client, callback=commons.statusInterval)
 loop = asyncio.get_event_loop()
 loop.run_forever()
